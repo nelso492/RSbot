6322b3009ad48e4a10adfa4265a7d4cd9c3542bc
Chad <chad.hollenbeck@owlcomputing.com>
2020-07-15 06:34:02 -0400

    init

--- rsbot.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-java -Xmx768m -XX:+UseG1GC -cp "C:\Users\chadh\Repos\Personal_Projects\powerbot\out;C:\Users\chadh\Repos\Personal_Projects\powerbot\resources\RsBot.jar" jagexappletviewer
-
-pause
